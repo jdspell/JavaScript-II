8abd43de31dc08b563198e4858426367e29b3d76
jdspell <spelljd15@gmail.com>
2019-01-22 18:03:21 -0500

    completed the closure assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function outerFxn() {
+  let privateVar = "This is a private variable.";
+  function innerFxn() {
+    console.log(privateVar);
+  }
+  innerFxn();
+}
+outerFxn();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 1;
+  return () => { return counter++; }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
